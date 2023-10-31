4a185359c1116f0c57e5ab5882125595f2d59607
Miguel Remedios <miguelremediios@gmail.com>
2023-10-31 01:36:10 +0000

    Project base structure, React Routing, Vite and Tailwindcss configurations

---
+++ src/_auth/forms/SignUpForm.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+const SignUpForm = () => {
+  return (
+    <div>
+      <Button>SignUp</Button>
+    </div>
+  );
+};
+
+export default SignUpForm;
